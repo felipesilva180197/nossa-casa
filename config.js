2e4edeaa4af3780e3c7d8d73f317600c38940da8
felipesilva180197 <felipesilva180197@gmail.com>
2026-09-16 14:48:00 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Dados do projeto Firebase "nossa-casa". Este arquivo não muda nas atualizações do sistema.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCXkrsx7vAEuoxkl3CPEiYX9deUZcbKFrc",
+  authDomain: "nossa-casa-ca091.firebaseapp.com",
+  projectId: "nossa-casa-ca091",
+  storageBucket: "nossa-casa-ca091.firebasestorage.app",
+  messagingSenderId: "571434598236",
+  appId: "1:571434598236:web:04f60282a234290a804b99"
+};
